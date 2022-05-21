fd6a9dc86905c2e85275d46be609079bc5fc371f
priyanka-bhoir <er.priyankabhoir1@gmail.com>
2022-05-21 17:29:21 +0530

    updated README.md

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-# Styling Text With Compose
+# Styling Text With JetPackCompose
 
 ## Font used
 ####
-Here i have used font provided by google fonts named [macondo_regular].(https://fonts.google.com/specimen/Macondo?preview.size=32) .
-if you want you can use any font provided
+#### Here i have used font provided by google fonts named [macondo_regular](https://fonts.google.com/specimen/Macondo?preview.size=32) .
+#### if you want you can use any font provided
 
 ## Output 
 
-![img.png](img.png)
+![img_2.png](img_2.png)
+
+## Key Learnings
+> 1. FontFamily -: you can diffren
+> 2. buildAnnotatedString
+> 3. withStyle
+> 4. textDecoration
+
+## Learning Source
+I Learned above topic from [youtube](https://www.youtube.com/watch?v=nm_LNJWHi9A&list=PLQkwcJG4YTCSpJ2NLhDTHhi6XBNfk9WiC)
+
+
+
+
 
--- app/src/main/java/com/priyanka/textstylewithcompose/MainActivity.kt
+++ app/src/main/java/com/priyanka/textstylewithcompose/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,15 +39,58 @@ class MainActivity : ComponentActivity() {
                     .fillMaxSize()
                     .background(Color(0xFF101010))
             ) {
-                Text(
-                    text = "The Great Priyanka",
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = FontFamily,
-                    fontStyle = FontStyle.Italic,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
-                )
+                Column() {
+                    Text(
+                        text = "The Great Priyanka",
+                        color = Color.White,
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily,
+                        fontStyle = FontStyle.Italic,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Green,
+                                    fontSize = 50.sp
+                                )
+                            ){
+                                append("T")
+                            }
+                            append("he")
+
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Green,
+                                    fontSize = 50.sp
+                                )
+                            ){
+                                append("G")
+                            }
+                            append("reat")
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Green,
+                                    fontSize = 50.sp
+                                )
+                            ){
+                                append("P")
+                            }
+                            append("riyanka")
+                        },
+                        color = Color.White,
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily,
+                        fontStyle = FontStyle.Italic,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        textDecoration = TextDecoration.Underline
+                    )
+                }
+
             }
         }
     }
